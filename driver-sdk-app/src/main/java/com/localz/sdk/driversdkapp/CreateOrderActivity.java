b557c82c3cd1bcc0c96ac307fb8b77683e3dd803
George Yamana <george.yamana@localz.co>
2018-09-01 00:25:35 +1000

    Updated create order to conform to JSONA

--- driver-sdk-app/src/main/java/com/localz/sdk/driversdkapp/CreateOrderActivity.java
+++ driver-sdk-app/src/main/java/com/localz/sdk/driversdkapp/CreateOrderActivity.java
@@ -1,20 +1,19 @@
 package com.localz.sdk.driversdkapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
 
 import com.localz.sdk.driver.Callback;
 import com.localz.sdk.driver.Error;
 import com.localz.sdk.driver.LocalzDriverSDK;
+import com.localz.sdk.driver.model.Address;
 import com.localz.sdk.driver.model.Order;
 import com.localz.sdk.driver.model.OrderStatus;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.HashMap;
 
 public class CreateOrderActivity extends AppCompatActivity {
 
@@ -25,7 +24,12 @@ public class CreateOrderActivity extends AppCompatActivity {
     private EditText deliveryName;
     private EditText deliveryEmail;
     private EditText deliveryPhone;
-    private EditText address;
+    private EditText subStreet;
+    private EditText street;
+    private EditText locality;
+    private EditText postcode;
+    private EditText region;
+    private EditText country;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +41,12 @@ public class CreateOrderActivity extends AppCompatActivity {
         deliveryName = findViewById(R.id.deliveryName);
         deliveryEmail = findViewById(R.id.deliveryEmail);
         deliveryPhone = findViewById(R.id.deliveryPhone);
-        address = findViewById(R.id.address);
+        subStreet = findViewById(R.id.sub_street);
+        street = findViewById(R.id.street);
+        locality = findViewById(R.id.locality);
+        postcode = findViewById(R.id.postcode);
+        region = findViewById(R.id.region);
+        country = findViewById(R.id.country);
 
         findViewById(R.id.submit).setOnClickListener(v -> {
             Order order = new Order();
@@ -53,8 +62,14 @@ public class CreateOrderActivity extends AppCompatActivity {
             order.deliveryName = deliveryName.getText().toString();
             order.deliveryEmail = deliveryEmail.getText().toString();
             order.deliveryPhone = deliveryPhone.getText().toString();
-            order.specific = new HashMap<>();
-            order.specific.put("address", address.getText().toString());
+            Address address = new Address();
+            address.subStreetAddress = subStreet.getText().toString();
+            address.streetAddress = street.getText().toString();
+            address.locality = locality.getText().toString();
+            address.postcode = postcode.getText().toString();
+            address.region = region.getText().toString();
+            address.country = country.getText().toString();
+            order.address = address;
 
             LocalzDriverSDK.getInstance().createOrder(CreateOrderActivity.this, order, new Callback<Order>() {
 
